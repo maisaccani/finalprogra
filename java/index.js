3fe8b18e8e66de5c0dccdd758b66de18e83c1ea0
Lara Saccani <larasaccani@MacBook-Air-de-maite.local>
2023-06-08 15:57:44 -0300

    intente conectar la web con la API pero me salta error en el fetch, despues lo sigo

---
+++ java/index.js
@@ -0,0 +1,32 @@
+const url= "https://api.allorigins.win/raw?url=https://api.deezer.com/chart";
+fetch (url)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    let arrayCanciones=data.tracks.data;
+    console.log(arrayCanciones);
+
+    let canciones1= document.querySelector(".canciones1");
+    let songs="";
+    for(let i=0; i<5;i++){
+
+        songs+= `<article class="lista_song">
+        <h3>
+            <a class="nombre_song" href="./detalle-cancion.html?id=${arrayCanciones[i].id}">${arrayCanciones[i].title}</a>
+        </h3>
+        <img src="${arrayCanciones[i].artist.picture}" alt="${arrayCanciones[i].title}" class="fotocancion">
+
+            <article class="bloque_nombre">
+                <a href="./detalle-artista.html?id=${arrayCanciones[i].artist.name}">${arrayCanciones[i].artist.name}</a>
+                <a href="./detalle-album.html?">${arrayCanciones[i].album.title}</a>
+            </article>
+         </article>`
+}
+canciones1.innerHTML= songs
+
+})
+.catch(function(error){
+    console.log( "Error: " + error);
+})
