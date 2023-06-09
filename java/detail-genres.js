7b43bf48ffd4ad8221b52f77a83070431502966c
Lara Saccani <larasaccani@MacBook-Air-de-maite.local>
2023-06-09 18:47:40 -0300

    termine el buscador

---
+++ java/detail-genres.js
@@ -0,0 +1,13 @@
+let formulario = document.querySelector('.form')
+
+formulario.addEventListener("submit", function(event){
+  event.preventDefault()
+
+  if(input.value == ""){
+      alert("Este campo es obligatorio")
+    } else if(input.value.length < 3){
+      alert("Este campo tiene que tener al menos 3 caracteres")
+    } else {
+      location.search = './search-results?search=' + input.value
+    }
+})
--- java/detalle-cancion.js
+++ java/detalle-cancion.js
@@ -1,3 +1,16 @@
+let formulario = document.querySelector('.form')
+
+formulario.addEventListener("submit", function(event){
+  event.preventDefault()
+
+  if(input.value == ""){
+      alert("Este campo es obligatorio")
+    } else if(input.value.length < 3){
+      alert("Este campo tiene que tener al menos 3 caracteres")
+    } else {
+      location.search = './search-results?search=' + input.value
+    }
+})
 const url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks"
 fetch(url)
 .then(function(response){
