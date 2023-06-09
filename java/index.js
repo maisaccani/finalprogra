43694655810b5cd683d9c5ab147d42bf8b14ad95
Lara Saccani <larasaccani@MacBook-Air-de-maite.local>
2023-06-09 10:48:06 -0300

    segui con el fetch de artistas

--- java/index.js
+++ java/index.js
@@ -25,7 +25,46 @@ fetch (url)
             </article>
          </article>`
 }
-canciones1.innerHTML= songs
+canciones1.innerHTML= songs;
+
+})
+.catch(function(error){
+    console.log( "Error: " + error);
+})
+
+
+
+
+//Artistas
+
+const url_artist = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27"
+
+fetch (url_artist)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    let arrayArtistas=data;
+    // console.log(arrayCanciones);
+
+    let artistas1= document.querySelector(".artistas1");
+    let artists="";
+    for(let i=0; i<5;i++){
+
+        artists+= `<article class="lista_artist">
+        <h3>
+            <a class="nombre_artist" href="./detalle-artista.html?id=${arrayArtistas[i].id}">${arrayArtistas[i].name}</a>
+        </h3>
+        <img src="${arrayArtistas[i].picture}" alt="${arrayArtistas[i].title}" class="foto_artista">
+
+            <article class="bloque_nombre">
+                <a href="./detalle-artista.html?id=${arrayArtistas[i].name}">${arrayCanciones[i].name}</a>
+                <a href="./detalle-album.html?">${arrayArtistas[i].nb_album.title}</a>
+            </article>
+         </article>`
+}
+canciones1.innerHTML= artists;
 
 })
 .catch(function(error){
