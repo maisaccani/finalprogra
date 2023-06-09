18d43d15974d62c3ca1ec3485ef6c80d316ab912
Trini Diaz Mantel <trinidiazmantel@Trinis-MacBook-Pro.local>
2023-06-09 14:37:40 -0300

    hice js de detalle artistas y detalle canciones

---
+++ java/detalle-artista.js
@@ -0,0 +1,24 @@
+const url_artist = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists"
+fetch(url_artist)
+.then(function(response){
+    return response.json()
+})
+
+.then(function(data){
+    console.log(data);
+
+    let artistas1= document.querySelector(".artistas1");
+    let arrayArtistas=data.data;
+    let artists="";
+
+    for(let i=0; i<5;i++){
+        artists += `<article class="lista_artist">
+        <img src= ${arrayArtistas[i].picture}>
+        <h2> ${arrayArtistas[i].name}</h2>
+        <h3> ${arrayArtistas[i].id}</h3>`
+    }
+    artistas1.innerHTML= artists;
+})
+.catch(function(error){
+    console.log( "Error: " + error);
+})
---
+++ java/detalle-cancion.js
@@ -0,0 +1,24 @@
+const url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks"
+fetch(url)
+.then(function(response){
+    return response.json();
+})
+
+.then(function(data){
+    console.log(data);
+    let canciones1= document.querySelector(".canciones1");
+    let arrayCanciones=data.data;
+    let songs="";
+
+    for(let i=0; i<5;i++){
+
+        songs+= `<article class="lista_song">
+        <img src="${arrayCanciones[i].artist.picture}" alt="${arrayCanciones[i].title}" class="fotocancion">
+        <h2> ${arrayCanciones[i].artist.name} </h2>
+        <h3> ${arrayCanciones[i].artist.name} </h3>`
+    }
+    canciones1.innerHTML= songs;
+})
+.catch(function(error){
+    console.log( "Error: " + error);
+})
