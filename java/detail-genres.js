1d290f06f49a59481db5c062e2602e38bf778204
Jazminsanchezz <sanchezj@udesa.edu.ar>
2023-06-11 13:05:24 -0300

    agregue el boton de aclarar

--- java/detail-genres.js
+++ java/detail-genres.js
@@ -11,3 +11,21 @@ formulario.addEventListener("submit", function(event){
       location.search = './search-results?search=' + input.value
     }
 })
+
+
+// boton para aclarar
+let botonclaro = document.querySelector('.botonclaro');
+let body = document.querySelector('body');
+let h2 = document.querySelector('h2');
+
+botonclaro.addEventListener('click', function() {
+  if (botonclaro.innerText == 'aclarar fondo') {
+    botonclaro.innerText = 'oscurecer fondo';
+    body.style.background = '#FFF';
+    h2.style.color = '#000';
+  } else {
+    botonclaro.innerText = 'aclarar fondo';
+    body.style.background = '#000';
+    h2.style.color = '#FFF';
+  }
+});
