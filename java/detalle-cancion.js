003a2135ad2fc21b24946b0c79bdfd74c5b79943
Jazminsanchezz <sanchezj@udesa.edu.ar>
2023-06-13 08:54:58 -0300

    mas cosas

--- java/detalle-cancion.js
+++ java/detalle-cancion.js
@@ -83,21 +83,31 @@ fetch(url)
 
 
 // boton para aclarar
+//let botonclaro = document.querySelector('.botonclaro');
+//let body = document.querySelector('body');
+//let h3 = document.querySelector('h3');
 let botonclaro = document.querySelector('.botonclaro');
-let body = document.querySelector('body');
-let h2 = document.querySelector('h2');
-let h1 = document.querySelector('h1');
+let section = document.querySelector('.detalles1');
+let h2 = document.querySelector('.titulo_detalle');
+let img = document.querySelector('.img-cancion-detalle');
+let h3 = document.querySelector('.nombre_cancion');
+
 
 botonclaro.addEventListener('click', function() {
-  if (botonclaro.innerText == 'aclarar fondo') {
+  if (botonclaro.innerText === 'aclarar fondo') {
     botonclaro.innerText = 'oscurecer fondo';
-    body.style.background = '#FFF';
+    detalles1.style.background = '#FFF';
     h2.style.color = '#000';
-    h1.style.color = '#000';
+    img.style.border = '1px solid #000';
+    h3.style.color = '#000';
+
   } else {
     botonclaro.innerText = 'aclarar fondo';
-    body.style.background = '#000';
-    h1.style.color = '#FFF';
-    h1.style.color = '#FFF';
+    detalles1.style.background = '#000';
+    h2.style.color = '#FFF';
+    img.style.border = '1px solid #FFF';
+    h3.style.color = '#FFF';
+
   }
 });
+
