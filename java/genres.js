02cc6305ca99467e16d29bf1caa830443c8fb219
Lara Saccani <larasaccani@MacBook-Air-de-maite.local>
2023-06-16 11:17:29 -0300

    hice pagina de detalle cancion

--- java/genres.js
+++ java/genres.js
@@ -14,6 +14,58 @@ let value=input.value.length
 })
 
 
+let recuperoFavCancion = localStorage.getItem("cancionesFavs");
+let favoritoscancion = JSON.parse(recuperoFavCancion);
+console.log(favoritoscancion);
+let seccioncanciones = document.getElementById("seccioncanciones");
+let canciont = document.getElementById('cancion')
+
+
+if (favoritoscancion == null || favoritoscancion.length == 0) {
+  peliculasTitle.innerHTML = "No agregaste canciones a la sección de favoritos";
+} else {
+  let allcanciones = "";
+
+
+  for (let i = 0; i < favoritoscancion.length; i++) {
+    let urlcancion =
+
+
+    fetch(urlcancion)
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(data) {
+        console.log(data);
+
+
+        let cancion = `<article class="lista_song">
+                            <h3>
+                                <a class="nombre_song" href="./detalle-cancion.html?id=${arrayCanciones[i].id}">${arrayCanciones[i].title}</a>
+                            </h3>
+                            <img src="${arrayCanciones[i].artist.picture}" alt="${arrayCanciones[i].title}" class="fotocancion">
+
+
+                                <article class="bloque_nombre">
+                                    <a href="./detalle-artista.html?id=${arrayCanciones[i].artist.name}">${arrayCanciones[i].artist.name}</a>
+                                </article>
+                            </article>`;
+        allcanciones += cancion;
+
+
+        seccioncanciones.innerHTML = allcanciones;
+      })
+      .catch(function(e) {
+        console.log(e);
+      });
+  }
+}
+
+
+
+
+
+
 // boton para aclarar
 let botonclaro = document.querySelector('.botonclaro');
 let body = document.querySelector('body');
