1f86f740c9387b12f57f8d39809243f9776670ca
Lara Saccani <larasaccani@MacBook-Air-de-maite.local>
2023-06-16 22:13:40 -0300

    intente hacer el buscador

---
+++ java/forms.js
@@ -0,0 +1,15 @@
+let form = document.querySelector("#buscador")
+let input = document.querySelector("#busqueda")
+let urlBuscador = ("https://api.allorigins.win/raw?url=API")
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let valor= input.value.length
+    if (valor == 0) {
+        alert("El campo esta vacío, ingrese una búsqueda!")
+    } else if (valor < 3) {
+        alert("El campo debe contener al menos 3 caracteres")
+    } else {
+        this.submit(); 
+    }
+});
